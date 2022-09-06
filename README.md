c44e1a1dbabb657715c6a1310d343f7df8dfb6c6
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-09-06 16:55:22 -0300

    feat: Improve user experience

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TermoWiki
-A Wiki termometer to evalute reliability of entries and their references
+
+A Wiki termometer to evalute reliability of Wikipedia entries and their references.
+
+As of September 2022, the tool is shaped for the Wikipedia in Portuguese.
--- popup/popup.js
+++ popup/popup.js
@@ -1,31 +1,33 @@
 const api = 'https://xtools.wmflabs.org/api/page';
 
 async function main() {
-  const revisions = document.getElementById("revisions");
-  const references = document.getElementById("references");
-  const negativeFlags = document.getElementById("negativeFlags");
-  const editors = document.getElementById("editors");
+  let revisions = document.getElementById("revisions");
+  let references = document.getElementById("references");
+  let editors = document.getElementById("editors");
+  let fullReference = document.getElementById("fullReference");
 
-  const queryOptions = { active: true, currentWindow: true };
-  const [{ url: urlStr }] = await chrome.tabs.query(queryOptions);
-  
-  const wikiRegex = /.*:\/\/.*\.wikipedia\.org\/.*/
+  let queryOptions = { active: true, currentWindow: true };
+  let [{ url: urlStr }] = await chrome.tabs.query(queryOptions);
+
+  let wikiRegex = /.*:\/\/.*\.wikipedia\.org\/.*/
   if (wikiRegex.test(urlStr)) {
-    const url = new URL(urlStr);
+    let url = new URL(urlStr);
 
-    const project = url.host;
-    const article = url.pathname.split('/')[2];
+    let project = url.host;
+    let article = url.pathname.split('/')[2];
 
-    const res1 = await fetch(`${api}/articleinfo/${project}/${article}`);
-    const articleInfo = await res1.json();
+    let res1 = await fetch(`${api}/articleinfo/${project}/${article}`);
+    let articleInfo = await res1.json();
 
     revisions.innerHTML = articleInfo.revisions;
     editors.innerHTML = articleInfo.editors;
 
-    const res2 = await fetch(`${api}/prose/${project}/${article}`);
-    const prose = await res2.json();
+    let res2 = await fetch(`${api}/prose/${project}/${article}`);
+    let prose = await res2.json();
 
-    references.innerHTML = prose.references;
+    let referenceUrl = `https://xtools.wmflabs.org/articleinfo/${project}/${article}?uselang=${project}`;
+
+    fullReference.href = referenceUrl;
   }
 }
 
